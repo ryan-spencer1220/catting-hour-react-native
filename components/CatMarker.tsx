f79c7a6de2df7fd4891672155acc510ed15761b1
Your Name <youremail@example.com>
2024-09-11 07:33:58 -0700

    move cat data to postgres database

--- components/CatMarker.tsx
+++ components/CatMarker.tsx
@@ -1,37 +1,13 @@
 import { CircleLayer, Images, ShapeSource, SymbolLayer } from '@rnmapbox/maps';
-import WhiteCat from '../assets/cat-icons/white-cat.png';
 import BlackCat from '../assets/cat-icons/black-cat.png';
-import BrownCat from '../assets/cat-icons/brown-cat.png';
-import CalicoCat from '../assets/cat-icons/calico-cat.png';
-import WhiteOrangeCat from '../assets/cat-icons/white-orange-cat.png';
-import OrangeWhiteCat from '../assets/cat-icons/orange-white.png';
-import SiameseCat from '../assets/cat-icons/siamese-cat.png';
-import TuxedoCat from '../assets/cat-icons/tuxedo-cat.png';
-import GrayWhiteCat from '../assets/cat-icons/gray-white.png';
-import GrayCat from '../assets/cat-icons/gray-cat.png';
 import { OnPressEvent } from '@rnmapbox/maps/lib/typescript/src/types/OnPressEvent';
-import data from '../data/cats.json';
 import { featureCollection, point } from '@turf/helpers';
 import { useCat } from '~/providers/CatProvider';
 
-const catIcons = {
-  'White Cat': 'WhiteCat',
-  'Black Cat': 'BlackCat',
-  'Brown Cat': 'BrownCat',
-  'Calico Cat': 'CalicoCat',
-  'White-Orange Cat': 'WhiteOrangeCat',
-  'Orange-White Cat': 'OrangeWhiteCat',
-  'Siamese Cat': 'SiameseCat',
-  'Tuxedo Cat': 'TuxedoCat',
-  'Gray-White Cat': 'GrayWhiteCat',
-  'Gray Cat': 'GrayCat',
-};
-
 const CatMarker = () => {
-  const points = data.map((cat) => point([cat.long, cat.lat], { cat }));
+  const { setSelectedCat, nearbyCats } = useCat();
+  const points = nearbyCats.map((cat) => point([cat.long, cat.lat], { cat }));
   const catsFeatures = featureCollection(points);
-
-  const { selectedCat, setSelectedCat } = useCat();
 
   const onPointPress = async (e: OnPressEvent) => {
     // console.log(JSON.stringify(e, null, 2));
--- providers/CatProvider.tsx
+++ providers/CatProvider.tsx
@@ -1,4 +1,6 @@
-import { useContext, createContext, PropsWithChildren, useState } from 'react';
+import { useContext, createContext, PropsWithChildren, useState, useEffect } from 'react';
+import { Alert } from 'react-native';
+import { supabase } from '~/utils/supabase';
 
 interface Cat {
   id: number;
@@ -13,14 +15,35 @@ interface Cat {
 interface CatContextType {
   selectedCat: Cat | undefined;
   setSelectedCat: React.Dispatch<React.SetStateAction<Cat | undefined>>;
+  nearbyCats: Array<Cat>;
 }
 
 const CatContext = createContext<CatContextType | undefined>(undefined);
 
 export default function CatProvider({ children }: PropsWithChildren) {
+  const [nearbyCats, setNearbyCats] = useState([]);
   const [selectedCat, setSelectedCat] = useState<Cat | undefined>();
+
+  useEffect(() => {
+    const fetchCats = async () => {
+      const { error, data } = await supabase.rpc('nearby_cats', {
+        lat: 0,
+        long: 0,
+      });
+
+      if (error) {
+        Alert.alert('Failed to fetch scooters...');
+      } else {
+        setNearbyCats(data);
+      }
+    };
+
+    fetchCats();
+  }, []);
   return (
-    <CatContext.Provider value={{ selectedCat, setSelectedCat }}>{children}</CatContext.Provider>
+    <CatContext.Provider value={{ selectedCat, setSelectedCat, nearbyCats }}>
+      {children}
+    </CatContext.Provider>
   );
 }
 
